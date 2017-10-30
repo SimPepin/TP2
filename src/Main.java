ed2dbae887fb0dc04357313d826582965e61cc03
SimPepin <32471293+SimPepin@users.noreply.github.com>
2017-10-30 11:21:53 -0400

    modif comment

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-
+		//Test pour les classes à ne pas prendre en considération et supprimer après.
 		Identification i = new Identification("Paul", "attend");
 		
 		Patient p = new Patient(26, i);
