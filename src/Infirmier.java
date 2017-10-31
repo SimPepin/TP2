02b78c9e94ab5a4342ce26b3559a03a97cec9b3a
SamiBalti <sami.balti@gmail.com>
2017-10-31 13:36:43 -0400

    TP 2 new merge

--- src/Infirmier.java
+++ src/Infirmier.java
@@ -1,4 +1,95 @@
 
 public class Infirmier {
 
+	
+	
+	
+	private Identification iD;
+	boolean disponible = true;	
+		
+	public Infirmier(String prenom, String nom) {
+		
+		this.iD = new Identification (prenom, nom);
+		this.disponible = true;
+		
+	}
+	public Infirmier(Identification iD, boolean disponibilité) {
+		
+		this.iD = iD;
+		this.disponible = disponibilité;
+		
+	}
+	/**
+	 * @return the iD
+	 */
+	public Identification getiD() {
+		return iD;
+	}
+
+	/**
+	 * @param The iD to set
+	 */
+	public void setiD(Identification iD) {
+		this.iD = iD;
+	}
+
+	/**
+	 * @return la disponibilité
+	 */
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	/**
+	 * @param Definir la disponibilité de l'infirmier
+	 */
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+
+	@Override
+	public String toString() {
+		return "Infirmier [iD=" + iD + ", disponible=" + disponible + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Infirmier other = (Infirmier) obj;
+		if (disponible != other.disponible)
+			return false;
+		if (iD == null) {
+			if (other.iD != null)
+				return false;
+		} else if (!iD.equals(other.iD))
+			return false;
+		return true;
+	}
+
+	public Infirmier clone(Infirmier infirmier){
+		
+		Identification copieID = this.getiD();
+		boolean copieDisponibilité = this.isDisponible();
+		Infirmier copieInfirmier = new Infirmier(copieID,copieDisponibilité);
+		return copieInfirmier;
+
+	}
+
+
+	
+
+
+
+	
+	
+	
+	
+	
+	
+	
 }
