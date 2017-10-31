0c93700dcd1a1bd08be039271a28706d3636c1a7
SimPepin <32471293+SimPepin@users.noreply.github.com>
2017-10-31 16:43:36 -0400

    Add files via upload

---
+++ src/Calendrier.java
@@ -0,0 +1,29 @@
+
+public class Calendrier {
+
+	public boolean ajouterRendezVous(RendezVous rdvAjout) {
+		return false;
+	}
+
+	public RendezVous obtenirProchainRendezVousPatient(Patient unPatient) {
+		return null;
+	}
+
+	public RendezVous obtenirProchainRendezVousInfirmier(Infirmier unInfirmier) {
+		return null;
+	}
+
+	public RendezVous obtenirProchainRendezVousDocteur(Docteur unDocteur) {
+		return null;
+	}
+
+	// Affiche comme la tête d'une file.
+	public void obtenirPLageHoraire(RendezVous rdvAfficher) {
+
+	}
+
+	public boolean annulerRendezVous(RendezVous rdvAnnule) {
+		return false;
+	}
+
+}
