aaa0c3228749e1140412ffc5e3a31a23f60e8b73
SimPepin <32471293+SimPepin@users.noreply.github.com>
2017-10-31 15:50:42 -0400

    test
    test

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		//Test pour les classes à ne pas prendre en considération et supprimer après.
 		System.out.println("test");
-		
+		System.out.println("test");
 
 	}
 
