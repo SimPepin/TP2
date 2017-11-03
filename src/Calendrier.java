2cbc22f7302f3f1e114c51053dd2dfdbac1e29fe
SimPepin <32471293+SimPepin@users.noreply.github.com>
2017-11-03 16:34:12 -0400

    c

--- src/Calendrier.java
+++ src/Calendrier.java
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 
 public class Calendrier {
-	
-	private PlageHoraire unHoraire;
-	private Calendrier next;
-	
-	
+		
 	
 	public boolean ajouterRendezVous(RendezVous rdvAjout) {
 		return false;
@@ -33,39 +28,3 @@ public class Calendrier {
 	}
 
 }
-=======
-
-public class Calendrier {
-	
-	private PlageHoraire unHoraire;
-	private Calendrier next;
-	
-	
-	
-	public boolean ajouterRendezVous(RendezVous rdvAjout) {
-		return false;
-	}
-
-	public RendezVous obtenirProchainRendezVousPatient(Patient unPatient) {
-		return null;
-	}
-
-	public RendezVous obtenirProchainRendezVousInfirmier(Infirmier unInfirmier) {
-		return null;
-	}
-
-	public RendezVous obtenirProchainRendezVousDocteur(Docteur unDocteur) {
-		return null;
-	}
-
-	// Affiche comme la tête d'une file.
-	public void obtenirPLageHoraire(RendezVous rdvAfficher) {
-
-	}
-
-	public boolean annulerRendezVous(RendezVous rdvAnnule) {
-		return false;
-	}
-
-}
->>>>>>> 65e5f5036e9ebd1f7dd1160017245ffc3304f252
